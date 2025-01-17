36e5755e77f465af1dd50dcea0d1cb6ee011e1db
Arthur Espana <artki_64@hotmail.com>
2025-01-17 18:10:20 -0400

    docs: Cambiados los comentarios para mayor claridad

--- P2-Even-Fibonacci.py
+++ P2-Even-Fibonacci.py
@@ -1,16 +1,13 @@
-first = 1
-second = 1
-even_fibonacci = 0
+# Definimos la variable primer y segundo con valor 1 porque la serie de fibonacci empieza con 1 y 1 luego estas variables se iran actualizando con los valores de la serie de fibonacci
+primer = 1
+segundo = 1
+valores_pares_fibonacci = 0 
 
-while second < 4000000:
-    if second % 2 == 0:
-        even_fibonacci += second
+while segundo < 4000000:
+    if segundo % 2 == 0: # Si el residuo de la división de segundo entre 2 es 0
+        valores_pares_fibonacci += segundo # Sumar segundo a la variable valores_pares_fibonacci porque es un número par
+    next_fib = primer + segundo    # Respetamos la secuencia de fibonacci
+    primer = segundo # Actualizamos el valor de primer con el valor de segundo, lo utilizaremos en la siguiente iteración
+    segundo = next_fib # Actualizamos el valor de segundo con el valor de next_fib, lo utilizaremos en la siguiente iteración
 
-    next_fib = first + second
-    first = second
-    second = next_fib
-
-print(even_fibonacci)
-
-
-# Use for en vez de while y estaba iterando uno a uno, tardaba demasiado
+print(valores_pares_fibonacci)
